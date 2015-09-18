1069652130206137c1379755c9901d82e55531f7
rdear4 <rdear4@gmail.com>
2015-09-18 12:54:12 -0400

    Testing test route

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/test', function(req, res) {
+
+	res.send("this is a test");
+
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
