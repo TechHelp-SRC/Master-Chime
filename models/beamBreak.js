d076cb7e587e156bbf5b2da375c8fe374ecec34b
Nick Chirico <nickchirico07@gmail.com>
2015-10-14 16:18:42 -0400

    added comments for my own sake, and stared to graso mongoose

--- models/beamBreak.js
+++ models/beamBreak.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
 var BeamBreak = new mongoose.Schema({
-	
+	//I was thinking when we define this we could have it be put in a group.
+	//like the 10:00 group.
+	//so when we output the data at the end, and we're packaging it up all nice
+	//we can just say amountOf(beambreaks.find({"date"=whatever,group = "10"}));
+	//this way we can just count them, and send just the hour and the corresponding #
+	//not sure how we're going to parse it.
+	//I'm assuing we can take just the date out of the date we put in below
 	timeStamp: { type: Date, default: Date.now }
 	
 });
